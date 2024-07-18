7dd641fca542ef3eda98d37309b4a3ec2732154f
solomonchidera <solochidera10@gmail.com>
2024-07-18 18:20:16 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" doc doc doc """
+""" Module Doc """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,10 +10,10 @@ from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """ DB Class """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """ Initialize a new DB instance """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -21,21 +21,23 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """ Function Doc """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """doc doc doc"""
+        """
+        Function Doc
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """doc doc doc"""
+        """ Function Doc """
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
         except NoResultFound:
@@ -45,7 +47,7 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """doc doc doc"""
+        """ Function Doc """
         user = self.find_user_by(id=user_id)
 
         for key, value in kwargs.items():
